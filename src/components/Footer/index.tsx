c976b288469ebc0ee11cae1c4a16a8f6ad720185
Nathan Wong <wkwong.nathan@gmail.com>
2024-10-22 15:25:01 +0800

    fix: update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import css from './styles.module.scss';
 export const Footer = () => {
     return (
         <div className={css.container}>
-            <a href="/">© Nathan Wong</a>
+            <a href="/">© Nathan Wong 2024</a>
         </div>
     );
 };
